8260e4c4090582802915b9877b6739ecfbe16c4c
im ranjitha <ranjithatk101@gmail.com>
2022-08-04 13:05:45 +0530

    changes

---
+++ src/components/BasicTable.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function BasicTable() {
+  return (
+    <div>
+      <table>
+        <thead>CAption</thead>
+        <tbody>body</tbody>
+      </table>
+    </div>
+  )
+}
+
+export default BasicTable
---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react"
+import SearchBar from "./SearchBar"
+import BasicTable from "./BasicTable"
+function Dashboard() {
+  return (
+    <div>
+      <SearchBar />
+      <BasicTable />
+    </div>
+  )
+}
+
+export default Dashboard
